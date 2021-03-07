897d2f51b9faa612e2570fb4aa7b566a5a27f561
Jason C Braatz <jason@jasoncbraatz.com>
2021-03-07 04:52:25 -0600

    blah

---
+++ jason.js
@@ -0,0 +1,12 @@
+/* let js = "amazing";
+console.log(40+50);
+console.log("Jason");
+console.log(23);
+// test
+// alert (typeof js);
+const jasonHairy = 'blahh'
+console.log(jasonHairy);
+console.log(typeof(jasonHairy));
+// jasonHairy = 'blah2';
+*/
+
