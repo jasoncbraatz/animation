9bf6d0bc607625ca138635347b0be4abd57a656a
Jason C Braatz <jason@jasoncbraatz.com>
2021-03-17 08:48:15 -0500

    updated code

--- jason.js
+++ jason.js
@@ -1,3 +1,4 @@
+"use strict";
 /* let js = "amazing";
 console.log(40+50);
 console.log("Jason");
@@ -9,4 +10,57 @@ console.log(jasonHairy);
 console.log(typeof(jasonHairy));
 // jasonHairy = 'blah2';
 */
+/*
+let jasonName = 'Jason'
+console.log (`${jasonName} is awesome`);
 
+console.log (`hello
+johhny`)
+
+if (jasonName == 'Jas1on')
+{
+    console.log(`OK now is good!`)
+}
+else {
+    console.log('never mind.')
+}
+
+// let j;
+// j = prompt("what?");
+// console.log (j);
+
+const dog='rufus'
+dog === 'fred' ? console.log('Poor. poor. fred.') : 
+console.log(`{$dog}`);
+
+console.log(`${dog}`);
+
+const doggie = 0 ? 'blah1' : 'blah2'
+console.log(doggie);
+
+const myFriends = ['Steph','Cap','Baby']
+console.log(myFriends)
+myFriends.push ('Fred')
+
+console.log(myFriends)
+*/
+
+const myObject = {
+  dog: "Fred",
+  cat: "Normpie",
+  Whoop: function () {
+    return this.dog + "Whoop";
+  },
+  house: function () {
+    return this.dog;
+  },
+};
+
+console.log(myObject.house());
+console.log(this.outerHeight);
+console.log(myObject.Whoop());
+
+// console.log (myObject.house())
+
+console.log(myObject.dog);
+console.log("Hello Doggy!");
